73a66f2ed933c5862175a60e9ad64551601c3877
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-07-16 06:34:35 +0200

    add how to play page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { HowToPlayComponent } from './pages/how-to-play/how-to-play.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'how-to-play', component: HowToPlayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
